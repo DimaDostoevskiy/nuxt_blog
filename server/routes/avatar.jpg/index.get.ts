47dd1d5390404450efacc09f15c1389b05bd0e9b
Dos <dostoevskiy@list.ru>
2026-03-06 15:33:00 +0300

    удалил лишние роуты

--- server/routes/avatar.jpg/index.get.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler, sendRedirect } from 'h3'
-import { useRuntimeConfig } from '#imports'
-
-export default defineEventHandler((event) => {
-  const base = (useRuntimeConfig().app?.baseURL as string) || '/'
-  const path = base.endsWith('/') ? base + 'avatar.jpg' : `${base}/avatar.jpg`
-  sendRedirect(event, path, 302)
-})
