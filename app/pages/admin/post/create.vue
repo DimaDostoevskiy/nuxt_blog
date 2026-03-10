89860d6618a70d0c1fee09a76d4a431c24a35a71
Dos <dostoevskiy@list.ru>
2026-03-10 21:10:04 +0300

    Изменил роут с "admin/blog" на "admin/post"

---
+++ app/pages/admin/post/create.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: ['auth'],
+})
+
+import type {IPostFormPayload} from '~/types/blog'
+
+const errorMessage = ref('')
+const form = ref<IPostFormPayload>({
+  id: undefined,
+  title: '',
+  description: '',
+  content: '',
+  published: false,
+  slug: '',
+  image: '',
+})
+const isLoading = ref(false)
+const isAutoGenerate = ref(false)
+const promptTheme = ref<string>('Например: Подсолнечное масло')
+
+const submit = async () => {
+  isLoading.value = true
+  errorMessage.value = ''
+  await $fetch('/api/posts/create', {
+    method: 'POST',
+    body: form.value,
+  } as Record<string, unknown>)
+      .then(() => {
+        navigateTo(`/admin/post`)
+      })
+      .catch((err) => {
+        errorMessage.value = err.data.message || 'Ошибка при создании поста'
+      })
+      .finally(() => {
+        isLoading.value = false
+      })
+}
+
+const fetchDeepSeek = () => {
+  isLoading.value = true
+  errorMessage.value = ''
+  $fetch('/api/ai/generate-post', {
+    method: 'POST',
+    body: {
+      promptTheme: promptTheme.value,
+    },
+  })
+      .then(async (res) => {
+        const data = res as any
+        if (data?.error) {
+          errorMessage.value = data.error.message
+        } else {
+          const generatePost = await parseResponse(data)
+          form.value.title = generatePost.title || ''
+          form.value.content = generatePost.content || ''
+          form.value.description = generatePost.description || ''
+        }
+      })
+      .catch(() => {
+        errorMessage.value = 'Ошибка! Не удалось сгенерировать пост!'
+      })
+      .finally(() => {
+        isLoading.value = false
+        isAutoGenerate.value = false
+      })
+}
+
+const parseResponse = async (data: any) => {
+  let content = data.choices[0].message.content
+  content = content.replace(/<think>[\s\S]*?<\/think>/g, '')
+  content = content.trim()
+  return JSON.parse(content)
+}
+</script>
+
+<template>
+  <LayoutDefaultSection :title="'Создать пост'">
+    <template #section-controls>
+      <NuxtLink :to="'/admin/post/'"
+                class="link-btn link-btn--outline"
+      >Назад к списку
+      </NuxtLink>
+    </template>
+    <template #section-content>
+      <KitImage :path="form.image || '/default_post_image.webp'"/>
+      <KitForm v-if="form && !isAutoGenerate"
+               submit-label="Создать"
+               :loading="isLoading"
+               :submit-disabled="!form.image?.trim()"
+               @submit="submit"
+      >
+        <KitImageUpload label="Изображение"
+                        required
+                        v-model="form.image"
+        />
+        <KitInput label="Заголовок"
+                  type="text"
+                  maxlength="255"
+                  required
+                  :debounce="200"
+                  v-model="form.title"
+        />
+        <KitInput label="Краткое описание"
+                  as="textarea"
+                  :rows="3"
+                  :debounce="200"
+                  v-model="form.description"
+        />
+        <KitInput label="Контент"
+                  as="textarea"
+                  :rows="12"
+                  required
+                  :debounce="200"
+                  v-model="form.content"
+        />
+        <KitInput label="Опубликовать пост"
+                  class="mb-4"
+                  type="checkbox"
+                  v-model="form.published"
+        />
+      </KitForm>
+      <KitForm v-if="isAutoGenerate"
+               submit-label="Создать"
+               :loading="isLoading"
+               :submit-disabled="!promptTheme.trim()"
+               @submit="fetchDeepSeek"
+      >
+        <KitInput label="Тема поста"
+                  type="text"
+                  maxlength="255"
+                  required
+                  :debounce="200"
+                  v-model="promptTheme"
+        />
+      </KitForm>
+      <KitAlert v-if="errorMessage"
+                title="Ошибка"
+                :text="errorMessage"
+                :type="'error'"
+      />
+    </template>
+  </LayoutDefaultSection>
+</template>
---
+++ app/pages/admin/post/edit/[id].vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import type {BlogPost, IPostFormPayload} from '~/types/blog'
+
+definePageMeta({
+  middleware: ['auth'],
+})
+
+useSeoMeta({
+  title: 'Редактировать пост — Админка',
+})
+
+const route = useRoute()
+const id = Number(route.params.id)
+
+const isLoading = ref(false)
+const message = ref('')
+const form = ref<IPostFormPayload>({
+  id: undefined,
+  title: '',
+  description: '',
+  content: '',
+  published: false,
+  slug: '',
+  image: '',
+})
+
+const {data: post, pending, error: fetchError} = await useFetch<BlogPost>(`/api/posts/${id}`)
+
+const submit = async () => {
+  message.value = ''
+  isLoading.value = true
+
+  await $fetch(`/api/posts/update`, {
+    method: 'POST',
+    body: form.value
+  } as Record<string, unknown>)
+      .then(() => {
+        navigateTo(`/admin/post`)
+      }).catch((err) => {
+        message.value = err.errorMessage || 'Ошибка!'
+      }).finally(() => {
+        isLoading.value = false
+      })
+}
+
+
+onMounted(() => {
+  if (post.value) {
+    form.value = {
+      id: post.value.id,
+      title: post.value.title,
+      description: String(post.value.description),
+      content: post.value.content,
+      published: post.value.published,
+      slug: post.value.slug,
+      image: post.value.image,
+    }
+  }
+})
+</script>
+
+<template>
+  <LayoutDefaultSection :title="'Создать пост'">
+    <template #section-controls>
+      <NuxtLink :to="'/admin/post/'"
+                class="link-btn link-btn--outline"
+      >Назад к списку
+      </NuxtLink>
+    </template>
+    <template #section-content>
+      <KitForm v-if="form"
+               :loading="isLoading"
+               submit-label="Сохранить"
+               @submit="submit">
+        <KitInput label="Заголовок"
+                  v-model="form.title"
+                  type="text"
+                  maxlength="255"
+                  required
+                  :debounce="0"
+        />
+        <KitInput label="Краткое описание"
+                  v-model="form.description"
+                  as="textarea"
+                  :rows="3"
+                  :debounce="0"
+        />
+        <KitInput label="Контент"
+                  v-model="form.content"
+                  as="textarea"
+                  :rows="12"
+                  required
+                  :debounce="0"
+        />
+        <KitImageUpload label="Изображение"
+                        v-model="form.image"
+        />
+        <KitInput
+            :label="'Опубликовано'"
+
+            type="checkbox"
+            v-model="form.published"
+        />
+      </KitForm>
+      <KitAlert v-if="pending"
+                :type="'success'"
+                :title="`Загрузка данных...`"
+                :text="`...`"
+      />
+      <KitAlert v-if="fetchError"
+                :type="'error'"
+                :title="`Ошибка!`"
+                :text="String(fetchError)"
+      />
+    </template>
+  </LayoutDefaultSection>
+</template>
